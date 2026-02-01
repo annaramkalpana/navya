feb5070e36d9b520fe78719e8d52b3cfd280a948
annaramkalpana <kalpanaannaram02@gmail.com>
2026-02-01 23:13:32 +0700

    Add configuration for database and email settings

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 
+SECRET_KEY = "super-secret-key"
+SQLALCHEMY_DATABASE_URI = "sqlite:///users.db"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = 'your_email@gmail.com'
+MAIL_PASSWORD = 'app_password'
