d2939327569d802860091a46f2a57801eef2c940
annaramkalpana <kalpanaannaram02@gmail.com>
2026-02-01 23:18:30 +0700

    Initialize Flask app with mail and login features
    Added Flask app setup with mail and login management.

--- main.py
+++ main.py
@@ -33,3 +33,27 @@ def role_required(role):
             return fn(*args, **kwargs)
         return decorated_view
     return wrapper
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_login import LoginManager, login_user, login_required, logout_user
+from flask_mail import Mail, Message
+from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import URLSafeTimedSerializer
+
+from config import *
+from models import db, User
+from decorators import role_required
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+db.init_app(app)
+mail = Mail(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+
+serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
