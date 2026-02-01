c8cd897e6ccf5ba5c16c0a98bd14c0b12eb4e7b3
annaramkalpana <kalpanaannaram02@gmail.com>
2026-02-01 23:15:54 +0700

    Add User model with SQLAlchemy integration

--- main.py
+++ main.py
@@ -8,3 +8,15 @@ MAIL_PORT = 587
 MAIL_USE_TLS = True
 MAIL_USERNAME = 'your_email@gmail.com'
 MAIL_PASSWORD = 'app_password'
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200))
+    role = db.Column(db.String(20))
+    is_verified = db.Column(db.Boolean, default=False)
